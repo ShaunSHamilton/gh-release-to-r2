67627ba55b0632705420bd2a5d7eba087127a63a
Shaun Hamilton <shauhami020@gmail.com>
2025-12-22 15:56:14 +0200

    feat: error handling and logs

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use aws_sdk_s3::{
     self as s3,
     primitives::{ByteStream, SdkBody},
 };
-use tracing::info;
+use tracing::{error, info};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 use crate::config::EnvVars;
@@ -10,7 +10,7 @@ use crate::config::EnvVars;
 mod config;
 
 #[tokio::main]
-async fn main() -> Result<(), s3::Error> {
+async fn main() -> Result<(), String> {
     tracing_subscriber::registry()
         .with(
             tracing_subscriber::EnvFilter::try_from_default_env()
@@ -45,31 +45,64 @@ async fn main() -> Result<(), s3::Error> {
 
     let s3 = s3::Client::new(&config);
     let gh = octocrab::instance();
-    let (owner, repo_name) = github_repository
-        .split_once('/')
-        .expect("GITHUB_REPOSITORY should contain '/'");
+    let (owner, repo_name) = match github_repository.split_once('/') {
+        Some(r) => r,
+        None => {
+            return Err(format!(
+                "failed to destructure GITHUB_REPOSITORY from {github_repository}"
+            ));
+        }
+    };
     let repo = gh.repos(owner, repo_name);
     // Get release from release_id
-    let release = repo.releases().get(release_id).await.unwrap();
+    let release = match repo.releases().get(release_id).await {
+        Ok(r) => r,
+        Err(e) => {
+            error!(release = release_id, ?e, "unable to get release");
+            return Err(e.to_string());
+        }
+    };
+    let version = match release.tag_name.split_once('/') {
+        Some((_, version)) => version,
+        None => return Err(format!("failed to get version from {}", release.tag_name)),
+    };
+
     let assets = release.assets;
     info!(n = assets.len(), "found assets");
     for asset in assets {
         info!(asset = asset.name, size = asset.size, "uploading asset");
+        let dest = format!("{}/{}", version, asset.name);
         // Download
-        let stream = repo.release_assets().stream(*asset.id).await.unwrap();
+        let stream = match repo.release_assets().stream(*asset.id).await {
+            Ok(s) => s,
+            Err(e) => {
+                error!(asset = asset.name, "unable to construct stream for asset");
+                return Err(e.to_string());
+            }
+        };
         // Convert into a streaming HTTP body
         let body = reqwest::Body::wrap_stream(stream);
         // Finally into ByteStream
         let byte_stream = ByteStream::new(SdkBody::from_body_1_x(body));
         // Upload
-        s3.put_object()
+        let res = s3
+            .put_object()
             .bucket(&bucket_name)
-            .key(asset.name)
+            .key(dest)
             .body(byte_stream)
             .content_length(asset.size)
             .send()
-            .await
-            .unwrap();
+            .await;
+
+        match res {
+            Ok(_) => {
+                info!(asset = asset.name, "successfully uploaded asset")
+            }
+            Err(e) => {
+                error!(asset = asset.name, "unable to upload asset");
+                return Err(e.to_string());
+            }
+        }
     }
 
     Ok(())
